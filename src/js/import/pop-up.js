29c39d6629c6c5fcc61b02e90c28bb9986095ead
Tim <ktn_96@icloud.com>
2020-07-30 16:23:07 +0300

    add pages

--- src/js/import/pop-up.js
+++ src/js/import/pop-up.js
@@ -5,6 +5,11 @@ $('a').on('click', function(event) {
     let href = $(this).attr('href');
     $(href).fadeIn();
     $('.pop-up-bg').fadeIn();
+    if(href === '#reviews') {
+      if($(window).width() < 767) {
+        $('.pop-up-bg').css({'top': '100px'});
+      }
+    }
   } 
   //close
   else if($(this).attr('rel') === 'modal:close') {
@@ -26,3 +31,21 @@ $('.pop-up-bg').on('click', function() {
     $('.pop-up-bg').css({'top': '0px'});
   }, 400);
 }); 
+
+
+
+
+//reviews
+$('.pop-up-order__reviews li').on('click', function() {
+  $(this).addClass('active');
+  $(this).prevAll('li').addClass('active');
+  $(this).nextAll('li').removeClass('active');
+});
+
+$('.pop-up-order__reviews li').hover(function() {
+  $(this).addClass('hover');
+  $(this).prevAll('li').addClass('hover');
+  $(this).nextAll('li').removeClass('hover');
+}, function() {
+  $('.pop-up-order__reviews li').removeClass('hover');
+});
